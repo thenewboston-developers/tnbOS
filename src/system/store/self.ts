52c0c81b0d983d1f0f51c8091afce24df6e8038a
bucky <buckyroberts@gmail.com>
2022-08-02 20:42:23 -0400

    25 - EditSelfModal updates

--- src/system/store/self.ts
+++ src/system/store/self.ts
@@ -1,5 +1,6 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, current, PayloadAction} from '@reduxjs/toolkit';
 
+import {IpcChannel} from 'shared/types';
 import {SYSTEM_SELF} from 'system/store/constants';
 import {Self} from 'system/types';
 import {setLocalAndStateReducer} from 'system/utils/ipc';
@@ -16,8 +17,12 @@ const self = createSlice({
   name: SYSTEM_SELF,
   reducers: {
     setSelf: setLocalAndStateReducer<Self>(SYSTEM_SELF),
+    updateSelf: (state: Self, {payload}: PayloadAction<Partial<Self>>) => {
+      Object.assign(state, payload);
+      window.electron.ipc.send(IpcChannel.setStoreValue, {key: SYSTEM_SELF, state: current(state)});
+    },
   },
 });
 
-export const {setSelf} = self.actions;
+export const {setSelf, updateSelf} = self.actions;
 export default self.reducer;
