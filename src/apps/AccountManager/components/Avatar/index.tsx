45a044c8630e5bbe917b1022b93df195d94d92d9
bucky <buckyroberts@gmail.com>
2022-08-07 13:31:24 -0400

    33 - Unify OnlineIndicator

--- src/apps/AccountManager/components/Avatar/index.tsx
+++ src/apps/AccountManager/components/Avatar/index.tsx
@@ -1,7 +1,4 @@
-import {mdiCircle} from '@mdi/js';
-import Icon from '@mdi/react';
-
-import {colors} from 'system/styles';
+import OnlineIndicator from 'system/components/OnlineIndicator';
 import {SFC} from 'system/types';
 import * as S from './Styles';
 
@@ -12,24 +9,10 @@ export interface AvatarProps {
 }
 
 const Avatar: SFC<AvatarProps> = ({className, displayImage, displayOnlineStatus = true, isOnline}) => {
-  const renderOnlineStatus = () => {
-    const color = isOnline ? colors.palette.green['400'] : colors.palette.gray['300'];
-    return (
-      <>
-        <S.OnlineIndicatorOuter>
-          <Icon color="white" path={mdiCircle} size="14px" />
-        </S.OnlineIndicatorOuter>
-        <S.OnlineIndicatorInner>
-          <Icon color={color} path={mdiCircle} size="10px" />
-        </S.OnlineIndicatorInner>
-      </>
-    );
-  };
-
   return (
     <S.Container className={className}>
       <S.Img alt="avatar" src={displayImage} />
-      {displayOnlineStatus && renderOnlineStatus()}
+      {displayOnlineStatus && <OnlineIndicator isOnline={isOnline} />}
     </S.Container>
   );
 };
