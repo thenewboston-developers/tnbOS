b1821726c3ebfe1d3e81d9285557f29462408c1c
bucky <buckyroberts@gmail.com>
2022-08-11 21:19:45 -0400

    Display API credit balance

--- src/apps/NetworkManager/components/NetworkCard/index.tsx
+++ src/apps/NetworkManager/components/NetworkCard/index.tsx
@@ -1,10 +1,11 @@
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {mdiDotsVertical} from '@mdi/js';
 
 import NetworkModal from 'apps/NetworkManager/modals/NetworkModal';
 import PopupMenu from 'system/components/DropdownMenu';
 import {deleteNetwork} from 'system/dispatchers/networks';
 import {useToggle} from 'system/hooks';
+import {getBalances} from 'system/selectors/state';
 import {AppDispatch, Network, SFC} from 'system/types';
 import {truncate} from 'system/utils/strings';
 import * as S from './Styles';
@@ -15,6 +16,7 @@ export interface NetworkCardProps {
 
 const NetworkCard: SFC<NetworkCardProps> = ({className, network}) => {
   const [networkModalIsOpen, toggleNetworkModal] = useToggle(false);
+  const balances = useSelector(getBalances);
   const dispatch = useDispatch<AppDispatch>();
 
   const handleDeleteNetwork = () => {
@@ -34,6 +36,7 @@ const NetworkCard: SFC<NetworkCardProps> = ({className, network}) => {
           <S.LeftText>
             <S.DisplayName>{network.displayName}</S.DisplayName>
             <S.NetworkId>{truncate(network.networkId, 24)}</S.NetworkId>
+            <S.Balance>Balance: {balances[network.networkId]}</S.Balance>
           </S.LeftText>
         </S.Left>
         <S.Right>
