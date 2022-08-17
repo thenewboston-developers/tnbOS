1009756dc0108fec0a9ed1fa9cc8d5a5235ea66e
bucky <buckyroberts@gmail.com>
2022-08-17 14:58:49 -0400

    Message interface

--- src/apps/Chat/types/messages.ts
+++ src/apps/Chat/types/messages.ts
@@ -4,3 +4,12 @@ export enum DeliveryStatus {
   pending = 'pending',
   received = 'received',
 }
+
+export interface Message {
+  content: string;
+  createdDate: string;
+  messageId: string;
+  modifiedDate: string;
+  recipient: string;
+  sender: string;
+}
