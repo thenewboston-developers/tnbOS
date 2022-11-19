ded35c149ec10637f616d4a3c732f2e631eb24b5
bucky <buckyroberts@gmail.com>
2022-11-19 13:44:22 -0500

    Timer functionality

--- src/apps/SpeedTest/containers/Styles.ts
+++ src/apps/SpeedTest/containers/Styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  max-height: 80%;
+  max-height: 90%;
   overflow-y: auto;
   padding: 24px;
   width: 60%;
