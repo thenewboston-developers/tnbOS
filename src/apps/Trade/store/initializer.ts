da28f595bd6217d2f8b8e4e189440c10fa1687ad
bucky <buckyroberts@gmail.com>
2023-01-07 18:35:36 -0500

    Testing offer sync updates

--- src/apps/Trade/store/initializer.ts
+++ src/apps/Trade/store/initializer.ts
@@ -2,7 +2,6 @@ import {
   TRADE_HOLDING_ACCOUNTS,
   TRADE_MANAGER,
   TRADE_OFFERS,
-  TRADE_OFFERS_SYNC,
   TRADE_ORDER_ERRORS,
   TRADE_ORDERS,
   TRADE_RECEIVING_ACCOUNTS,
@@ -12,7 +11,6 @@ import {
 import {initialState as holdingAccountsInitialState, setHoldingAccounts} from 'apps/Trade/store/holdingAccounts';
 import {initialState as managerInitialState, setManager} from 'apps/Trade/store/manager';
 import {initialState as offersInitialState, setOffers} from 'apps/Trade/store/offers';
-import {initialState as offersSyncInitialState, setOffersSync} from 'apps/Trade/store/offersSync';
 import {initialState as orderErrorsInitialState, setOrderErrors} from 'apps/Trade/store/orderErrors';
 import {initialState as ordersInitialState, setOrders} from 'apps/Trade/store/orders';
 import {initialState as receivingAccountsInitialState, setReceivingAccounts} from 'apps/Trade/store/receivingAccounts';
@@ -25,7 +23,6 @@ const loadAppData = (dispatch: AppDispatch, store: LocalElectronStore): void => 
   const holdingAccounts = store?.[TRADE_HOLDING_ACCOUNTS] || holdingAccountsInitialState;
   const manager = store?.[TRADE_MANAGER] || managerInitialState;
   const offers = store?.[TRADE_OFFERS] || offersInitialState;
-  const offersSync = store?.[TRADE_OFFERS_SYNC] || offersSyncInitialState;
   const orderErrors = store?.[TRADE_ORDER_ERRORS] || orderErrorsInitialState;
   const orders = store?.[TRADE_ORDERS] || ordersInitialState;
   const receivingAccounts = store?.[TRADE_RECEIVING_ACCOUNTS] || receivingAccountsInitialState;
@@ -34,7 +31,6 @@ const loadAppData = (dispatch: AppDispatch, store: LocalElectronStore): void => 
   dispatch(setHoldingAccounts(holdingAccounts));
   dispatch(setManager(manager));
   dispatch(setOffers(offers));
-  dispatch(setOffersSync(offersSync));
   dispatch(setOrderErrors(orderErrors));
   dispatch(setOrders(orders));
   dispatch(setReceivingAccounts(receivingAccounts));
