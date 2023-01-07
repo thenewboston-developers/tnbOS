bbd7a165c5adf001ae9b5a8a7ea8f0ca927d3ec2
bucky <buckyroberts@gmail.com>
2023-01-07 18:58:29 -0500

    Revert sync logic

--- src/apps/Trade/store/offersSync.ts
+++ src/apps/Trade/store/offersSync.ts
@@ -1,7 +1,9 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createSlice, current, PayloadAction} from '@reduxjs/toolkit';
 
 import {TRADE_OFFERS_SYNC} from 'apps/Trade/store/constants';
 import {OffersSync, SyncRecipient} from 'apps/Trade/types';
+import {IpcChannel} from 'shared/types';
+import {setLocalAndStateReducer} from 'system/utils/ipc';
 
 export const initialState: OffersSync = {
   modifiedDate: null,
@@ -15,13 +17,16 @@ const offersSync = createSlice({
     resetOffersSync: (state: OffersSync, {payload}: PayloadAction<string>) => {
       state.modifiedDate = payload;
       state.recipients = {};
+      window.electron.ipc.send(IpcChannel.setStoreValue, {key: TRADE_OFFERS_SYNC, state: current(state)});
     },
+    setOffersSync: setLocalAndStateReducer<OffersSync>(TRADE_OFFERS_SYNC),
     setOffersSyncRecipient: (state: OffersSync, {payload}: PayloadAction<SyncRecipient>) => {
       const {accountNumber} = payload;
       state.recipients[accountNumber] = payload;
+      window.electron.ipc.send(IpcChannel.setStoreValue, {key: TRADE_OFFERS_SYNC, state: current(state)});
     },
   },
 });
 
-export const {resetOffersSync, setOffersSyncRecipient} = offersSync.actions;
+export const {resetOffersSync, setOffersSync, setOffersSyncRecipient} = offersSync.actions;
 export default offersSync.reducer;
