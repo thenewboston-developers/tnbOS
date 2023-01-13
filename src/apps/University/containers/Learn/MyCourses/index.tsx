0a71b36e8951bbfaee3920e4a6b7dc94dd13af50
bucky <buckyroberts@gmail.com>
2023-01-12 20:25:36 -0500

    My courses UI work

--- src/apps/University/containers/Learn/MyCourses/index.tsx
+++ src/apps/University/containers/Learn/MyCourses/index.tsx
@@ -1,8 +1,54 @@
+import CourseCard from 'apps/University/components/CourseCard';
+import CourseCardsContainer from 'apps/University/components/CourseCardsContainer';
+import EmptyPage from 'system/components/EmptyPage';
 import {SFC} from 'system/types';
+
+import MyCoursesEmptyStateGraphic from './assets/my-courses-empty-state.png';
 import * as S from './Styles';
 
 const MyCourses: SFC = ({className}) => {
-  return <S.Container className={className}>My Courses</S.Container>;
+  const courses = [
+    {
+      courseId: 'def456',
+      description: 'Hamburger pastrami meatball drumstick brisket shoulder, boudin andouille.',
+      instructor: 'aaa7484c7c5f41901606631a771fcae7873cae2edac78c5597ba1472a1874dd6',
+      name: 'Java Game Development',
+      thumbnailUrl: 'https://i.imgur.com/tBMibH7.png',
+    },
+    {
+      courseId: 'abc123',
+      description: 'Hamburger pastrami meatball drumstick brisket shoulder, boudin andouille.',
+      instructor: 'aaa7484c7c5f41901606631a771fcae7873cae2edac78c5597ba1472a1874dd6',
+      name: 'Visual Basic',
+      thumbnailUrl: 'https://i.imgur.com/8mRjXLF.png',
+    },
+  ];
+
+  const handleClick = (courseId: string) => {
+    console.log(courseId);
+  };
+
+  const renderCourseCards = () => {
+    return courses.map((course) => (
+      <CourseCard course={course} key={course.courseId} onClick={() => handleClick(course.courseId)} />
+    ));
+  };
+
+  const renderPageContent = () => {
+    if (!!courses.length) {
+      return <CourseCardsContainer>{renderCourseCards()}</CourseCardsContainer>;
+    }
+
+    return (
+      <EmptyPage
+        bottomText="Courses from connected accounts will appear here."
+        graphic={MyCoursesEmptyStateGraphic}
+        topText="Nothing here!"
+      />
+    );
+  };
+
+  return <S.Container className={className}>{renderPageContent()}</S.Container>;
 };
 
 export default MyCourses;
--- src/apps/University/containers/Learn/Navigation/index.tsx
+++ src/apps/University/containers/Learn/Navigation/index.tsx
@@ -1,11 +1,27 @@
-import {SFC} from 'system/types';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {getActiveLearnPage} from 'apps/University/selectors/state';
+import {setActiveLearnPage} from 'apps/University/store/manager';
+import {LearnPage} from 'apps/University/types';
+import {AppDispatch, SFC} from 'system/types';
 import * as S from './Styles';
 
 const Navigation: SFC = ({className}) => {
+  const activeLearnPage = useSelector(getActiveLearnPage);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleClick = (learnPage: LearnPage) => {
+    dispatch(setActiveLearnPage(learnPage));
+  };
+
   return (
     <S.Container className={className}>
-      <S.Item isActive={true}>BROWSE</S.Item>
-      <S.Item>MY COURSES</S.Item>
+      <S.Item isActive={activeLearnPage === LearnPage.browse} onClick={() => handleClick(LearnPage.browse)}>
+        BROWSE
+      </S.Item>
+      <S.Item isActive={activeLearnPage === LearnPage.myCourses} onClick={() => handleClick(LearnPage.myCourses)}>
+        MY COURSES
+      </S.Item>
     </S.Container>
   );
 };
