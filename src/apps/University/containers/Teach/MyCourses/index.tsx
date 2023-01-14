f1fb061f5b8d262f0768e556af8c3b20fef401b7
bucky <buckyroberts@gmail.com>
2023-01-13 21:35:06 -0500

    Course modal

--- src/apps/University/containers/Teach/MyCourses/index.tsx
+++ src/apps/University/containers/Teach/MyCourses/index.tsx
@@ -1,10 +1,13 @@
 import {useTaughtCourses} from 'apps/University/hooks';
+import CourseModal from 'apps/University/modals/CourseModal';
+import {useToggle} from 'system/hooks';
 import {SFC} from 'system/types';
 
 import Course from './Course';
 import * as S from './Styles';
 
 const MyCourses: SFC = ({className}) => {
+  const [courseModalIsOpen, toggleCourseModal] = useToggle(false);
   const taughtCourses = useTaughtCourses();
 
   const renderCourses = () => {
@@ -12,15 +15,23 @@ const MyCourses: SFC = ({className}) => {
     return <S.Courses>{courses}</S.Courses>;
   };
 
+  const renderCourseModal = () => {
+    if (!courseModalIsOpen) return null;
+    return <CourseModal close={toggleCourseModal} />;
+  };
+
   const renderNewCourseButton = () => {
-    return <button>New Course</button>;
+    return <button onClick={toggleCourseModal}>New Course</button>;
   };
 
   return (
-    <S.Container className={className}>
-      <S.SectionHeading heading="My Courses" rightContent={renderNewCourseButton()} />
-      {renderCourses()}
-    </S.Container>
+    <>
+      <S.Container className={className}>
+        <S.SectionHeading heading="My Courses" rightContent={renderNewCourseButton()} />
+        {renderCourses()}
+      </S.Container>
+      {renderCourseModal()}
+    </>
   );
 };
 
---
+++ src/apps/University/modals/CourseModal/index.tsx
@@ -0,0 +1,93 @@
+import {useMemo} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Form, Formik} from 'formik';
+
+import Modal from 'apps/University/components/Modal';
+import {setCourse} from 'apps/University/store/courses';
+import {setActiveTeachCourseId, setActiveTeachPage} from 'apps/University/store/manager';
+import {Course, PublicationStatus, TeachPage} from 'apps/University/types';
+import Button, {ButtonType} from 'system/components/Button';
+import {Input} from 'system/components/FormElements';
+import {getSelf} from 'system/selectors/state';
+import {AppDispatch, SFC} from 'system/types';
+import {currentSystemDate} from 'system/utils/dates';
+import yup from 'system/utils/forms/yup';
+
+interface CourseModalProps {
+  close(): void;
+}
+
+const CourseModal: SFC<CourseModalProps> = ({className, close}) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const self = useSelector(getSelf);
+
+  const initialValues = {
+    description: '',
+    name: '',
+    thumbnailUrl: '',
+  };
+
+  type FormValues = typeof initialValues;
+
+  const handleSubmit = (values: FormValues) => {
+    try {
+      const courseId = crypto.randomUUID();
+
+      const course: Course = {
+        courseId,
+        createdDate: currentSystemDate(),
+        description: values.description,
+        instructor: self.accountNumber,
+        name: values.name,
+        publicationStatus: PublicationStatus.draft,
+        thumbnailUrl: values.thumbnailUrl,
+      };
+
+      dispatch(setCourse(course));
+      dispatch(setActiveTeachCourseId(courseId));
+      dispatch(setActiveTeachPage(TeachPage.courseDetails));
+
+      close();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // TODO: Proper validation
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      description: yup.string(),
+      name: yup.string(),
+      thumbnailUrl: yup.string(),
+    });
+  }, []);
+
+  return (
+    <Modal className={className} close={close} header="New Course">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validateOnMount={false}
+        validationSchema={validationSchema}
+      >
+        {({dirty, errors, isSubmitting, touched, isValid}) => (
+          <Form>
+            <Input errors={errors} label="Thumbnail URL" name="thumbnailUrl" touched={touched} />
+            <Input errors={errors} label="Name" name="name" touched={touched} />
+            <Input errors={errors} label="Description" name="description" touched={touched} />
+            <Button
+              dirty={dirty}
+              disabled={isSubmitting}
+              isSubmitting={isSubmitting}
+              isValid={isValid}
+              text="Submit"
+              type={ButtonType.submit}
+            />
+          </Form>
+        )}
+      </Formik>
+    </Modal>
+  );
+};
+
+export default CourseModal;
