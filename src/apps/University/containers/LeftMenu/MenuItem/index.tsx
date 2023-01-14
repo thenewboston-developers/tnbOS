5a85ce7af1bcbfdc6001d8a51b3cd6cc4f4cf245
bucky <buckyroberts@gmail.com>
2023-01-14 03:09:15 -0500

    Left menu updates

--- src/apps/University/containers/LeftMenu/MenuItem/index.tsx
+++ src/apps/University/containers/LeftMenu/MenuItem/index.tsx
@@ -1,6 +1,7 @@
+import {useMemo} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
-import {getActivePage} from 'apps/University/selectors/state';
+import {getActiveLearnCourseId, getActivePage, getEnrollments} from 'apps/University/selectors/state';
 import {setActivePage} from 'apps/University/store/manager';
 import {Page} from 'apps/University/types';
 import {AppDispatch, SFC} from 'system/types';
@@ -13,10 +14,33 @@ export interface MenuItemProps {
 }
 
 const MenuItem: SFC<MenuItemProps> = ({children, className, icon, page}) => {
+  const activeLearnCourseId = useSelector(getActiveLearnCourseId);
   const activePage = useSelector(getActivePage);
   const dispatch = useDispatch<AppDispatch>();
+  const enrollments = useSelector(getEnrollments);
 
-  const isActivePage = activePage === page;
+  const isActivePage = useMemo(() => {
+    if (page === activePage) return true;
+
+    const isEnrolledInActiveLearnCourse = !!enrollments[activeLearnCourseId];
+
+    if (page === Page.learnBrowse) {
+      if (activePage === Page.learnCourseHome && !isEnrolledInActiveLearnCourse) return true;
+      if (activePage === Page.learnCourseLecture && !isEnrolledInActiveLearnCourse) return true;
+    }
+
+    if (page === Page.learnMyCourses) {
+      if (activePage === Page.learnCourseHome && isEnrolledInActiveLearnCourse) return true;
+      if (activePage === Page.learnCourseLecture && isEnrolledInActiveLearnCourse) return true;
+    }
+
+    if (page === Page.teachMyCourses) {
+      const teachPages = [Page.teachCourseDetails, Page.teachCourseLectureDetails, Page.teachCourseLectures];
+      if (teachPages.includes(activePage)) return true;
+    }
+
+    return false;
+  }, [activeLearnCourseId, activePage, enrollments, page]);
 
   const handleClick = () => {
     dispatch(setActivePage(page));
