fd6aad3554b094aa94d98365b4ac81f2a8f11a11
bucky <buckyroberts@gmail.com>
2023-01-16 20:15:55 -0500

    Remove test data

--- src/apps/University/store/lectures.ts
+++ src/apps/University/store/lectures.ts
@@ -1,57 +1,11 @@
 import {createSlice, current, PayloadAction} from '@reduxjs/toolkit';
 
 import {UNIVERSITY_LECTURES} from 'apps/University/store/constants';
-import {Lecture, Lectures, PublicationStatus} from 'apps/University/types';
+import {Lecture, Lectures} from 'apps/University/types';
 import {IpcChannel} from 'shared/types';
 import {setLocalAndStateReducer} from 'system/utils/ipc';
 
-export const initialState: Lectures = {
-  computergamedevelopmentL1: {
-    courseId: 'computergamedevelopment',
-    createdDate: '2022-12-22T00:45:10Z',
-    description:
-      'In this first lecture we learn about the history of games. In this first lecture we learn about the history of games. In this first lecture we learn about the history of games.',
-    lectureId: 'computergamedevelopmentL1',
-    name: 'The History of Computer Game Development',
-    position: 1,
-    publicationStatus: PublicationStatus.published,
-    thumbnailUrl: 'https://i.imgur.com/QkVxS3m.png',
-    youtubeId: 'XCMWDfwUINA',
-  },
-  computergamedevelopmentL2: {
-    courseId: 'computergamedevelopment',
-    createdDate: '2022-12-22T00:45:10Z',
-    description: 'In this second lecture we learn more about things.',
-    lectureId: 'computergamedevelopmentL2',
-    name: 'Second Computer Game Development Lecture',
-    position: 2,
-    publicationStatus: PublicationStatus.published,
-    thumbnailUrl: 'https://i.imgur.com/QkVxS3m.png',
-    youtubeId: 'IcNtuCWkWpo',
-  },
-  mysqlL1: {
-    courseId: 'mysql',
-    createdDate: '2022-12-22T00:45:10Z',
-    description: 'This is the first class in MySQL.',
-    lectureId: 'mysqlL1',
-    name: 'The History of MySQL',
-    position: 1,
-    publicationStatus: PublicationStatus.published,
-    thumbnailUrl: 'https://i.imgur.com/wdXAd3X.png',
-    youtubeId: 'XCMWDfwUINA',
-  },
-  mysqlL2: {
-    courseId: 'mysql',
-    createdDate: '2022-12-22T00:45:10Z',
-    description: 'In this second lecture we learn more about things.',
-    lectureId: 'mysqlL2',
-    name: 'Second MySQL Lecture',
-    position: 2,
-    publicationStatus: PublicationStatus.published,
-    thumbnailUrl: 'https://i.imgur.com/wdXAd3X.png',
-    youtubeId: 'IcNtuCWkWpo',
-  },
-};
+export const initialState: Lectures = {};
 
 const lectures = createSlice({
   initialState,
