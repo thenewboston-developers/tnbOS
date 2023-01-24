4733d7b1a3263b026baf7bcf700904b148e9b5f8
bucky <buckyroberts@gmail.com>
2023-01-23 21:40:42 -0500

    Get lecture record validators

--- src/apps/University/listeners/getLectureRecordListener.ts
+++ src/apps/University/listeners/getLectureRecordListener.ts
@@ -1,13 +1,34 @@
+import {getLectureRecordValidator, validateCourse} from 'apps/University/validators/getLectureRecordValidators';
 import {Block} from 'shared/types';
+import store from 'system/store';
 import {AppDispatch} from 'system/types';
 import {displayErrorToast} from 'system/utils/toast';
 
-const getLectureRecordListener = (block: Block, dispatch: AppDispatch, networkId: string) => {
+const getLectureRecordListener = (block: Block, _: AppDispatch, networkId: string) => {
   (async () => {
     try {
       const {payload, sender: blockSender} = block;
       const {params} = payload;
-      console.log(params);
+      const {
+        system: {self},
+        university: {courses, lectureRecords},
+      } = store.getState();
+
+      await getLectureRecordValidator.validate(params);
+      const {courseId} = params;
+
+      validateCourse(courseId, courses, self);
+
+      const lectureRecord = lectureRecords[courseId];
+
+      if (lectureRecord) {
+        // TODO: send back lecture record
+        console.log(blockSender);
+        console.log(networkId);
+        console.log('----');
+        console.log(courseId);
+        console.log(lectureRecord);
+      }
     } catch (error) {
       console.error(error);
       displayErrorToast('Invalid block received');
---
+++ src/apps/University/validators/getLectureRecordValidators.ts
@@ -0,0 +1,15 @@
+import {Courses, PublicationStatus} from 'apps/University/types';
+import {courseIdSchema} from 'apps/University/utils/yup';
+import {Self} from 'system/types';
+import yup from 'system/utils/yup';
+
+export const getLectureRecordValidator = yup.object({
+  courseId: courseIdSchema,
+});
+
+export const validateCourse = (courseId: string, courses: Courses, self: Self) => {
+  const course = courses[courseId];
+  if (!course) throw new Error('That course does not exist');
+  if (course.instructor !== self.accountNumber) throw new Error('Block recipient is not instructor of that course');
+  if (course.publicationStatus !== PublicationStatus.published) throw new Error('Course is not published');
+};
