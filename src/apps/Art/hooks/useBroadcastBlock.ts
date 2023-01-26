ffb967169a2ef32a839c1d18e2d01588465617e7
bucky <buckyroberts@gmail.com>
2023-01-25 21:03:14 -0500

    Filter broadcast recipients

--- src/apps/Art/hooks/useBroadcastBlock.ts
+++ src/apps/Art/hooks/useBroadcastBlock.ts
@@ -1,18 +1,23 @@
 import {useCallback} from 'react';
+import {useSelector} from 'react-redux';
 
 import {setQueuedBlocksBlock} from 'apps/Art/blocks';
 import {QueuedBlock} from 'apps/Art/types';
 import {useConnectedAccounts} from 'system/hooks';
+import {getSelf} from 'system/selectors/state';
 import {displayErrorToast} from 'system/utils/toast';
 
 const useBroadcastBlock = () => {
   const connectedAccounts = useConnectedAccounts();
+  const self = useSelector(getSelf);
 
   const connectedAccountNumbers = Object.keys(connectedAccounts);
 
   return useCallback(
     async (block: QueuedBlock) => {
       for (const accountNumber of connectedAccountNumbers) {
+        if (![accountNumber, self.accountNumber].includes(block.payload.owner)) continue;
+
         try {
           await setQueuedBlocksBlock({
             networkId: connectedAccounts[accountNumber],
@@ -25,7 +30,7 @@ const useBroadcastBlock = () => {
         }
       }
     },
-    [connectedAccountNumbers, connectedAccounts],
+    [connectedAccountNumbers, connectedAccounts, self.accountNumber],
   );
 };
 
