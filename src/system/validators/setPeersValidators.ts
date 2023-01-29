f8598b5c2291b030962ace5e79e8c554c515b247
bucky <buckyroberts@gmail.com>
2023-01-29 16:24:28 -0500

    Add noUnknown to system validators

--- src/system/validators/setPeersValidators.ts
+++ src/system/validators/setPeersValidators.ts
@@ -1,9 +1,11 @@
 import yup from 'system/utils/yup';
 
-export const setPeersValidator = yup.object({
-  correlation_id: yup.string().required(),
-  return_value: yup
-    .mixed()
-    .nullable()
-    .test('is-null', 'Return value is not null', (return_value: any) => return_value === null),
-});
+export const setPeersValidator = yup
+  .object({
+    correlation_id: yup.string().required(),
+    return_value: yup
+      .mixed()
+      .nullable()
+      .test('is-null', 'Return value is not null', (return_value: any) => return_value === null),
+  })
+  .noUnknown();
