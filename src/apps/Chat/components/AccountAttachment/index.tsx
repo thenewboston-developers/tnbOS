00d5a977d9f3e28bbb2942eb0d4c8828e28e1b8e
bucky <buckyroberts@gmail.com>
2023-02-06 21:45:22 -0500

    Set account on button click

--- src/apps/Chat/components/AccountAttachment/index.tsx
+++ src/apps/Chat/components/AccountAttachment/index.tsx
@@ -1,9 +1,11 @@
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {mdiArrowRightBoldCircleOutline, mdiCheckBold} from '@mdi/js';
 
 import {getAccounts} from 'system/selectors/state';
-import {Account, SFC} from 'system/types';
+import {setAccount} from 'system/store/accounts';
+import {Account, AppDispatch, SFC} from 'system/types';
 import {truncate} from 'system/utils/strings';
+import NoImage from './assets/no-image.png';
 import * as S from './Styles';
 
 export interface AccountAttachmentProps {
@@ -12,42 +14,62 @@ export interface AccountAttachmentProps {
 
 const AccountAttachment: SFC<AccountAttachmentProps> = ({attachedAccount, className}) => {
   const accounts = useSelector(getAccounts);
+  const dispatch = useDispatch<AppDispatch>();
 
   const localAccount = accounts[attachedAccount.accountNumber];
 
-  const renderAvatar = (account: Account) => (
-    <S.AlignCenter>
-      <S.Img alt="avatar" src={account.displayImage} />
-    </S.AlignCenter>
-  );
+  const hasDifferences =
+    attachedAccount.displayImage !== localAccount?.displayImage ||
+    attachedAccount.displayName !== localAccount?.displayName;
 
-  const renderAvatarURL = (account: Account) => (
-    <S.AlignCenter>
-      <S.Label>Avatar URL</S.Label>
-      <div>{truncate(account.displayImage, 16)}</div>
-    </S.AlignCenter>
-  );
+  const handleIconClick = () => {
+    if (!hasDifferences) return;
+    dispatch(setAccount(attachedAccount));
+  };
+
+  const renderAvatar = (account?: Account) => {
+    const src = account?.displayImage ? account.displayImage : NoImage;
+
+    return (
+      <S.AlignCenter>
+        <S.Img alt="avatar" src={src} />
+      </S.AlignCenter>
+    );
+  };
+
+  const renderAvatarURL = (account?: Account) => {
+    const url = account?.displayImage ? truncate(account.displayImage, 16) : '-';
+
+    return (
+      <S.AlignCenter>
+        <S.Label>Avatar URL</S.Label>
+        <div>{url}</div>
+      </S.AlignCenter>
+    );
+  };
 
   const renderCenter = () => {
-    const hasDifferences =
-      attachedAccount.displayImage !== localAccount.displayImage ||
-      attachedAccount.displayName !== localAccount.displayName;
-
     const path = hasDifferences ? mdiArrowRightBoldCircleOutline : mdiCheckBold;
 
     return (
       <S.Center>
-        <S.Icon $hasDifferences={hasDifferences} path={path} size="48px" />
+        <div onClick={handleIconClick}>
+          <S.Icon $hasDifferences={hasDifferences} path={path} size="48px" />
+        </div>
       </S.Center>
     );
   };
 
-  const renderDisplayName = (account: Account) => (
-    <S.AlignCenter>
-      <S.Label>Display Name</S.Label>
-      <div>{truncate(account.displayName, 16)}</div>
-    </S.AlignCenter>
-  );
+  const renderDisplayName = (account?: Account) => {
+    const displayName = account?.displayName ? truncate(account.displayName, 16) : '-';
+
+    return (
+      <S.AlignCenter>
+        <S.Label>Display Name</S.Label>
+        <div>{displayName}</div>
+      </S.AlignCenter>
+    );
+  };
 
   return (
     <S.Container className={className}>
