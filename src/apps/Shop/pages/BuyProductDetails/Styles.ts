14a135cc9a66d65448215bf3dd08abb623d0b5b2
bucky <buckyroberts@gmail.com>
2023-02-10 19:21:28 -0500

    Working on buy product details page

--- src/apps/Shop/pages/BuyProductDetails/Styles.ts
+++ src/apps/Shop/pages/BuyProductDetails/Styles.ts
@@ -1,3 +1,45 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import UBack from 'apps/Shop/components/Back';
+
+export const Back = styled(UBack)`
+  margin-bottom: 24px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+`;
+
+export const Description = styled.div`
+  color: #777;
+  margin-top: 16px;
+`;
+
+export const Img = styled.img`
+  border-radius: 4px;
+`;
+
+export const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 50%;
+`;
+
+export const MainContent = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+export const Name = styled.div`
+  color: #222;
+  font-size: 32px;
+  font-weight: 700;
+  margin-top: 4px;
+`;
+
+export const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
--- src/apps/Shop/pages/BuyProductDetails/index.tsx
+++ src/apps/Shop/pages/BuyProductDetails/index.tsx
@@ -1,11 +1,45 @@
+import {useDispatch} from 'react-redux';
+
 import {useActiveBuyProduct} from 'apps/Shop/hooks';
-import {SFC} from 'system/types';
+import {setActivePage} from 'apps/Shop/store/manager';
+import {Page} from 'apps/Shop/types';
+import {AppDispatch, SFC} from 'system/types';
 import * as S from './Styles';
 
 const BuyProductDetails: SFC = ({className}) => {
   const activeBuyProduct = useActiveBuyProduct();
+  const dispatch = useDispatch<AppDispatch>();
 
-  return <S.Container className={className}>{activeBuyProduct?.productId}</S.Container>;
+  const handleBackClick = () => {
+    dispatch(setActivePage(Page.buyHome));
+  };
+
+  const renderLeft = () => {
+    return (
+      <S.Left>
+        <S.Img alt="Product image" src={activeBuyProduct?.imageUrl} />
+      </S.Left>
+    );
+  };
+
+  const renderRight = () => {
+    return (
+      <S.Right>
+        <S.Name>{activeBuyProduct?.name || '-'}</S.Name>
+        <S.Description>{activeBuyProduct?.description || '-'}</S.Description>
+      </S.Right>
+    );
+  };
+
+  return (
+    <S.Container className={className}>
+      <S.Back onClick={handleBackClick}>Back to products</S.Back>
+      <S.MainContent>
+        {renderLeft()}
+        {renderRight()}
+      </S.MainContent>
+    </S.Container>
+  );
 };
 
 export default BuyProductDetails;
