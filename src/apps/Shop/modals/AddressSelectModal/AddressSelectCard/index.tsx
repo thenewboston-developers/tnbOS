94332a35221b744ab8c77bed08e33e28870e97e2
bucky <buckyroberts@gmail.com>
2023-02-11 20:06:07 -0500

    Address select logic

--- src/apps/Shop/modals/AddressSelectModal/AddressSelectCard/index.tsx
+++ src/apps/Shop/modals/AddressSelectModal/AddressSelectCard/index.tsx
@@ -1,14 +1,17 @@
 import {getName} from 'country-list';
 
 import {Address} from 'apps/Shop/types';
+import {GenericVoidFunction} from 'shared/types';
 import {SFC} from 'system/types';
 import * as S from './Styles';
 
 export interface AddressSelectCardProps {
   address: Address;
+  isSelected: boolean;
+  onClick: GenericVoidFunction;
 }
 
-const AddressSelectCard: SFC<AddressSelectCardProps> = ({address, className}) => {
+const AddressSelectCard: SFC<AddressSelectCardProps> = ({address, className, isSelected, onClick}) => {
   const {address1, address2, city, countryCode, fullName, state, zipCode} = address;
 
   const renderCountryName = () => {
@@ -16,7 +19,7 @@ const AddressSelectCard: SFC<AddressSelectCardProps> = ({address, className}) =>
   };
 
   return (
-    <S.Container className={className}>
+    <S.Container className={className} isSelected={isSelected} onClick={onClick}>
       <div>{fullName}</div>
       <div>{address1}</div>
       {address2 ? <div>{address2}</div> : null}
--- src/apps/Shop/modals/AddressSelectModal/index.tsx
+++ src/apps/Shop/modals/AddressSelectModal/index.tsx
@@ -1,4 +1,4 @@
-import {useMemo} from 'react';
+import {useMemo, useState} from 'react';
 import {useSelector} from 'react-redux';
 
 import {getAddresses} from 'apps/Shop/selectors/state';
@@ -12,25 +12,43 @@ interface AddressSelectModalProps {
 }
 
 const AddressSelectModal: SFC<AddressSelectModalProps> = ({className, close}) => {
+  const [selectedAddressId, setSelectedAddressId] = useState<string | null>(null);
   const addresses = useSelector(getAddresses);
 
   const addressList = useMemo(() => {
     return Object.values(addresses);
   }, [addresses]);
 
-  const handleClick = () => {
-    console.log('Click');
+  const handleAddressSelectCardClick = (addressId: string) => {
+    if (addressId === selectedAddressId) {
+      setSelectedAddressId(null);
+    } else {
+      setSelectedAddressId(addressId);
+    }
+  };
+
+  const handleButtonClick = () => {
+    console.log(selectedAddressId);
+    close();
   };
 
   const renderAddressSelectCards = () => {
-    const _addresses = addressList.map((address) => <AddressSelectCard address={address} key={address.addressId} />);
+    const _addresses = addressList.map((address) => (
+      <AddressSelectCard
+        address={address}
+        key={address.addressId}
+        isSelected={address.addressId === selectedAddressId}
+        onClick={() => handleAddressSelectCardClick(address.addressId)}
+      />
+    ));
+
     return <S.AddressSelectCards>{_addresses}</S.AddressSelectCards>;
   };
 
   return (
     <S.Modal className={className} close={close} header="Select Address">
       {renderAddressSelectCards()}
-      <S.Button onClick={handleClick} text="Submit" />
+      <S.Button disabled={!selectedAddressId} onClick={handleButtonClick} text="Submit" />
     </S.Modal>
   );
 };
