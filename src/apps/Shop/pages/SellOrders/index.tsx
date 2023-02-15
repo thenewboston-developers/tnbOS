fddbfc454570080b7cf14bf859e7439ae89e5bb5
bucky <buckyroberts@gmail.com>
2023-02-14 22:53:00 -0500

    Order filtering

--- src/apps/Shop/pages/SellOrders/index.tsx
+++ src/apps/Shop/pages/SellOrders/index.tsx
@@ -5,15 +5,18 @@ import orderBy from 'lodash/orderBy';
 import EmptyText from 'apps/Shop/components/EmptyText';
 import Order from 'apps/Shop/components/Order';
 import {getOrders} from 'apps/Shop/selectors/state';
+import {getSelf} from 'system/selectors/state';
 import {SFC} from 'system/types';
 import * as S from './Styles';
 
 const SellOrders: SFC = ({className}) => {
   const orders = useSelector(getOrders);
+  const self = useSelector(getSelf);
 
   const orderList = useMemo(() => {
-    return orderBy(Object.values(orders), ['createdDate'], ['desc']);
-  }, [orders]);
+    const _orders = Object.values(orders).filter(({seller}) => seller === self.accountNumber);
+    return orderBy(_orders, ['createdDate'], ['desc']);
+  }, [orders, self.accountNumber]);
 
   const renderOrders = () => {
     const _orders = orderList.map((order) => <Order key={order.orderId} order={order} />);
