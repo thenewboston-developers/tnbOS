1896956e78b6365c04378caae4bb0b33c40b3389
bucky <buckyroberts@gmail.com>
2023-02-17 19:34:56 -0500

    Search for courses

--- src/apps/University/pages/LearnBrowse/index.tsx
+++ src/apps/University/pages/LearnBrowse/index.tsx
@@ -1,5 +1,5 @@
-import {useMemo} from 'react';
-import {useDispatch} from 'react-redux';
+import {ChangeEvent, useMemo, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import orderBy from 'lodash/orderBy';
 
 import CourseCard from 'apps/University/components/CourseCard';
@@ -8,22 +8,44 @@ import {useAvailableCourses} from 'apps/University/hooks';
 import {setActiveLearnCourseId, setActivePage} from 'apps/University/store/manager';
 import {Page} from 'apps/University/types';
 import EmptyPage from 'system/components/EmptyPage';
+import {getAccounts} from 'system/selectors/state';
 import {AppDispatch, SFC} from 'system/types';
 
 import LearnBrowseEmptyStateGraphic from './assets/browse-empty-state.png';
 import * as S from './Styles';
 
 const LearnBrowse: SFC = ({className}) => {
+  const [searchText, setSearchText] = useState<string>('');
+  const accounts = useSelector(getAccounts);
   const availableCourses = useAvailableCourses();
   const dispatch = useDispatch<AppDispatch>();
 
   const courseList = useMemo(() => {
-    return orderBy(availableCourses, ['modifiedDate'], ['desc']);
-  }, [availableCourses]);
+    let results = availableCourses;
+
+    if (searchText) {
+      const lowerCaseSearchText = searchText.toLowerCase();
+
+      results = results.filter(({description, instructor, name}) => {
+        const account = accounts[instructor];
+        return (
+          account?.displayName?.toLowerCase().includes(lowerCaseSearchText) ||
+          description.toLowerCase().includes(lowerCaseSearchText) ||
+          name.toLowerCase().includes(lowerCaseSearchText)
+        );
+      });
+    }
+
+    return orderBy(results, ['modifiedDate'], ['desc']);
+  }, [accounts, availableCourses, searchText]);
 
   const handleClick = (courseId: string) => {
     dispatch(setActiveLearnCourseId(courseId));
     dispatch(setActivePage(Page.learnCourseHome));
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
   };
 
   const renderCourseCards = () => {
@@ -46,7 +68,14 @@ const LearnBrowse: SFC = ({className}) => {
     );
   };
 
-  return <S.Container className={className}>{renderPageContent()}</S.Container>;
+  return (
+    <S.Container className={className}>
+      <S.Search>
+        <S.Input onChange={handleInputChange} placeholder="Search courses..." type="text" />
+      </S.Search>
+      <S.PageContent>{renderPageContent()}</S.PageContent>
+    </S.Container>
+  );
 };
 
 export default LearnBrowse;
